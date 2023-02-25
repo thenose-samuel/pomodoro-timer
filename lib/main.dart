144a27209185612542ae6c482088410eb9e348ed
Samuel Khongthaw <skthaw1@gmail.com>
2023-02-25 18:16:08 +0530

    added timer widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Timer());
+  runApp(const PomodoroTimer());
 }
 
-class Timer extends StatelessWidget {
-  const Timer({super.key});
+class PomodoroTimer extends StatelessWidget {
+  const PomodoroTimer({super.key});
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -28,12 +31,12 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          SizedBox(
-            height: 20,
+          const SizedBox(
+            height: 50,
           ),
           Padding(
               padding: EdgeInsets.only(left: 15, right: 15), child: TopBar()),
-          SizedBox(
+          const SizedBox(
             height: 20,
           ),
           Body(),
@@ -50,17 +53,20 @@ class TopBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      // color: Colors.red,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: const [
           Text(
-            'Hi, User',
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold , fontSize: 30),
+            'Hi, Samuel',
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),
           ),
           Text(
             '25.02.2023',
-            style: TextStyle(color: Color(0xFF858584), fontSize: 18),
+            style: TextStyle(
+                color: Color(0xFF858584),
+                fontSize: 15,
+                fontWeight: FontWeight.bold),
           )
         ],
       ),
@@ -82,33 +88,104 @@ class Body extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            SizedBox(
+            const SizedBox(
               height: 20,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Container(
-                  padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(40)), color: Colors.red[300],),
-                  child: Column(
-                    children: [
-                     const Text(
-                        '25:00',
-                        style: TextStyle(color: Colors.white, fontSize: 90, fontWeight: FontWeight.bold),
-                      ),
-                      Container(
-                        decoration: const BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(5)), color: Colors.white,),
-                        padding: const EdgeInsets.all(15),
-                        child: const Text('START', style: TextStyle(color: Colors.red, fontSize: 20, fontWeight: FontWeight.bold),),
-                      )
-                    ],
-                  )
-                )
+                    padding: const EdgeInsets.only(
+                        top: 10, bottom: 10, left: 30, right: 30),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(40)),
+                      color: Colors.red[300],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        CountdownTimer(),
+                        GestureDetector(
+                          onTap: () => {},
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              borderRadius: BorderRadius.all(Radius.circular(5)),
+                              color: Colors.white,
+                            ),
+                            padding: const EdgeInsets.all(10),
+                            child: const Text(
+                              'START',
+                              style: TextStyle(
+                                  color: Colors.red,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        )
+                      ],
+                    ))
               ],
-            )
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            // const Text('inspired by pomofocus.io', style: TextStyle(color: Colors.white, fontSize: 10, fontWeight: FontWeight.bold))
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CountdownTimer extends StatefulWidget {
+  const CountdownTimer({Key? key}) : super(key: key);
+  @override
+  State<CountdownTimer> createState() => _CountdownTimerState();
+}
+
+class _CountdownTimerState extends State<CountdownTimer> {
+  Timer? countDownTimer;
+  Duration initialTime = const Duration(minutes: 25);
+  late int remainingSeconds = initialTime.inSeconds.remainder(60);
+  late int remainingMinutes = initialTime.inMinutes;
+  @override
+  void initState() {
+    startTimer();
+    super.initState();
+  }
+
+  void startTimer() {
+    countDownTimer =
+        Timer.periodic(const Duration(seconds: 1), (_) => countDown());
+  }
+
+  void countDown() {
+    setState(() {
+      final seconds = initialTime.inSeconds - 1;
+      if (seconds < 0) {
+        countDownTimer!.cancel();
+      } else {
+        initialTime = Duration(seconds: seconds);
+        remainingMinutes = initialTime.inMinutes;
+        remainingSeconds = initialTime.inSeconds.remainder(60);
+      }
+      log('${(remainingMinutes < 10)? '0': null} $remainingMinutes:${(remainingSeconds < 10)? '0': null}$remainingSeconds', name: 'time');
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 10.0),
+      child: Text(
+        '${(remainingMinutes < 10)? '0': ''} $remainingMinutes:${(remainingSeconds < 10)? '0': ''}$remainingSeconds',
+        style: const TextStyle(
+            color: Colors.white, fontSize: 90, fontWeight: FontWeight.bold),
       ),
     );
   }
