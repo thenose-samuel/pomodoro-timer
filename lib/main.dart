8b2bcb0c4c7e21a733520cd2775cbeedc03a2e39
Samuel Khongthaw <skthaw1@gmail.com>
2023-03-31 10:20:50 +0530

    added short break

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'dart:async';
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:pomodoro/colors.dart';
 import 'package:pomodoro/state.dart';
@@ -25,7 +22,7 @@ class PomodoroTimer extends StatelessWidget {
         ChangeNotifierProvider(
             create: (context) => AppState(page: CurrentPage.pomodoro)),
         ChangeNotifierProvider(create: (context) => TimerState()),
-      ], child: HomePage()),
+      ], child: const HomePage()),
     );
   }
 }
@@ -37,16 +34,27 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [
-          const SizedBox(
-            height: 50,
+        children: const [
+          SizedBox(
+            height: 70,
           ),
           Padding(
               padding: EdgeInsets.only(left: 15, right: 15), child: TopBar()),
-          const SizedBox(
+          SizedBox(
+            height: 40,
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Body(),
+          ),
+          SizedBox(
             height: 20,
           ),
-          Body(),
+          Text('inspired by pomofocus.io',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold))
         ],
       ),
       backgroundColor: const Color(0xFF1a1a1a),
@@ -59,24 +67,22 @@ class TopBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: const [
-          Text(
-            'Hi, Samuel',
-            style: TextStyle(
-                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),
-          ),
-          Text(
-            '25.02.2023',
-            style: TextStyle(
-                color: Color(0xFF858584),
-                fontSize: 15,
-                fontWeight: FontWeight.bold),
-          )
-        ],
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: const [
+        Text(
+          'Hi, Marjiba',
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),
+        ),
+        // Text(
+        //   '25.02.2023',
+        //   style: TextStyle(
+        //       color: Color(0xFF858584),
+        //       fontSize: 15,
+        //       fontWeight: FontWeight.bold),
+        // )
+      ],
     );
   }
 }
@@ -86,77 +92,93 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Consumer<AppState>(
-        builder: (context, page, child) => AnimatedContainer(
-          curve: Curves.fastOutSlowIn,
-          decoration: BoxDecoration(
-              color: page.backgroundColor,
-              borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(35), topRight: Radius.circular(35))),
-          duration: const Duration(seconds: 1),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 20,
+    return Consumer<AppState>(
+      builder: (context, page, child) => AnimatedContainer(
+        height: 350,
+        curve: Curves.fastOutSlowIn,
+        decoration: BoxDecoration(
+            color: page.backgroundColor,
+            borderRadius: const BorderRadius.all(Radius.circular(35))),
+        duration: const Duration(seconds: 1),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              Consumer<TimerState>(
+                builder: (context, timer, _) => GestureDetector(
+                  onTap: () {
+                    timer.pause(pause: true);
+                    page.changeTheme(CurrentPage.pomodoro);
+                    timer.setInitialTime(const Duration(minutes: 25));
+                  },
+                  child: Text('Pomodoro',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: page.page == CurrentPage.pomodoro
+                              ? Colors.white
+                              : Colors.grey,
+                          fontSize: 20)),
+                ),
               ),
-              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-                Text('Pomodoro',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        fontSize: 20)),
-                GestureDetector(
-                    onTap: () => {
-
+              Consumer<TimerState>(
+                builder: (context, timer, _) => GestureDetector(
+                    onTap: () {
+                      timer.pause(pause: true);
+                      page.changeTheme(CurrentPage.shortBreak);
+                      timer.setInitialTime(const Duration(minutes: 5));
                     },
                     child: Text('Short Break',
                         style: TextStyle(
                             fontWeight: FontWeight.bold,
-                            color: Colors.grey,
+                            color: page.page == CurrentPage.shortBreak
+                                ? Colors.white
+                                : Colors.grey,
                             fontSize: 20))),
-                Text('Long Break',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey,
-                        fontSize: 20)),
-              ]),
-              const SizedBox(
-                height: 20,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  AnimatedContainer(
-                      duration: const Duration(seconds: 1),
-                      curve: Curves.fastOutSlowIn,
-                      padding: const EdgeInsets.only(
-                          top: 10, bottom: 10, left: 30, right: 30),
-                      decoration: BoxDecoration(
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(40)),
-                        color: page.containerColor,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          CountdownTimer(),
-                          SizedBox(
-                            height: 20,
-                          )
-                        ],
-                      ))
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              // const Text('inspired by pomofocus.io', style: TextStyle(color: Colors.white, fontSize: 10, fontWeight: FontWeight.bold))
-            ],
-          ),
+              // GestureDetector(
+              //   onTap: () => {page.changeTheme(CurrentPage.shortBreak)},
+              //   child: const Text('Long Break',
+              //       style: TextStyle(
+              //           fontWeight: FontWeight.bold,
+              //           color: Colors.grey,
+              //           fontSize: 20)),
+              // ),
+            ]),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                AnimatedContainer(
+                    duration: const Duration(seconds: 1),
+                    curve: Curves.fastOutSlowIn,
+                    padding: const EdgeInsets.only(
+                        top: 10, bottom: 10, left: 30, right: 30),
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(40)),
+                      color: page.containerColor,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const [
+                        CountdownTimer(),
+                        SizedBox(
+                          height: 20,
+                        )
+                      ],
+                    ))
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
         ),
       ),
     );
@@ -179,7 +201,7 @@ class _CountdownTimerState extends State<CountdownTimer> {
             padding: const EdgeInsets.only(right: 10.0),
             child: Consumer<TimerState>(
               builder: (context, timer, child) => Text(
-                '${(timer.remainingMinutes < 10) ? '0' : ''} ${timer.remainingMinutes}:${(timer.remainingSeconds < 10) ? '0' : ''}${timer.remainingSeconds}',
+                '${(timer.remainingMinutes < 10) ? '0' : ''}${timer.remainingMinutes}:${(timer.remainingSeconds < 10) ? '0' : ''}${timer.remainingSeconds}',
                 style: const TextStyle(
                     color: Colors.white,
                     fontSize: 90,
@@ -191,14 +213,24 @@ class _CountdownTimerState extends State<CountdownTimer> {
             builder: (context, timer, child) => GestureDetector(
               onTap: () {
                 if (!timer.timerRunning) {
+                  // if (page.page == CurrentPage.shortBreak) {
+                  //   timer.initialTime = const Duration(minutes: 5);
+                  // }
                   timer.startTimer();
                 } else {
                   timer.pause(pause: true);
                 }
-                if (page.page == CurrentPage.pomodoro) {
+                // page.changeTheme(CurrentPage.timerRunning);
+                if (page.page == CurrentPage.shortBreak ||
+                    page.page == CurrentPage.pomodoro) {
                   page.changeTheme(CurrentPage.timerRunning);
+                  return;
                 } else {
-                  page.changeTheme(CurrentPage.pomodoro);
+                  if (page.prevPage == CurrentPage.pomodoro) {
+                    page.changeTheme(CurrentPage.pomodoro);
+                  } else {
+                    page.changeTheme(CurrentPage.shortBreak);
+                  }
                 }
               },
               child: Container(
@@ -210,8 +242,11 @@ class _CountdownTimerState extends State<CountdownTimer> {
                 child: Text(
                   (!timer.timerRunning) ? 'START' : 'PAUSE',
                   style: TextStyle(
-                      color:
-                          (!timer.timerRunning) ? Colors.red : Colors.black87,
+                      color: (!timer.timerRunning)
+                          ? (page.page == CurrentPage.pomodoro)
+                              ? AppColors.startPomodoro
+                              : AppColors.startShortBreak
+                          : Colors.black87,
                       fontSize: 20,
                       fontWeight: FontWeight.bold),
                 ),
--- lib/state.dart
+++ lib/state.dart
@@ -3,39 +3,40 @@ import 'dart:async';
 import 'package:pomodoro/colors.dart';
 import 'package:flutter/material.dart';
 
-enum CurrentPage {
-  pomodoro,
-  shortBreak,
-  timerRunning
-}
+enum CurrentPage { pomodoro, shortBreak, timerRunning }
 
-
-class AppState extends ChangeNotifier{
+class AppState extends ChangeNotifier {
   late CurrentPage page;
+  late CurrentPage prevPage;
   late Color backgroundColor;
   late Color containerColor;
-  AppState({required this.page}){
-   changeTheme(page);
+  late Color startTextColor;
+  AppState({required this.page}) {
+    changeTheme(page);
   }
 
-  void changeTheme(CurrentPage page){
-    if(page == CurrentPage.pomodoro){
+  void changeTheme(CurrentPage page) {
+    prevPage = this.page;
+    if (page == CurrentPage.pomodoro) {
       backgroundColor = AppColors.backgroundRed;
       containerColor = AppColors.containerRed;
+      startTextColor = Colors.black38;
       this.page = page;
       notifyListeners();
       return;
     }
-    if(page == CurrentPage.shortBreak){
+    if (page == CurrentPage.shortBreak) {
       backgroundColor = AppColors.backgroundBlue;
       containerColor = AppColors.containerBlue;
+      startTextColor = Colors.black38;
       this.page = page;
       notifyListeners();
       return;
     }
-    if(page == CurrentPage.timerRunning){
+    if (page == CurrentPage.timerRunning) {
       backgroundColor = AppColors.backgroundDim;
       containerColor = Colors.transparent;
+      startTextColor = Colors.black38;
       this.page = page;
       notifyListeners();
       return;
@@ -43,31 +44,42 @@ class AppState extends ChangeNotifier{
   }
 }
 
-class TimerState extends ChangeNotifier{
+class TimerState extends ChangeNotifier {
   Timer? countDownTimer;
   Duration initialTime = const Duration(minutes: 25);
   late int remainingSeconds = initialTime.inSeconds.remainder(60);
   late int remainingMinutes = initialTime.inMinutes;
   bool timerRunning = false;
 
+  void setInitialTime(Duration init) {
+    initialTime = init;
+    remainingMinutes = initialTime.inMinutes;
+    remainingSeconds = initialTime.inSeconds.remainder(60);
+    notifyListeners();
+  }
+
   void startTimer() {
     timerRunning = true;
     countDownTimer =
         Timer.periodic(const Duration(seconds: 1), (_) => countDown());
+    notifyListeners();
   }
+
   void pause({required bool pause}) {
-      countDownTimer!.cancel();
-      timerRunning = false;
+    countDownTimer!.cancel();
+    timerRunning = false;
+    notifyListeners();
   }
+
   void countDown() {
-      final seconds = initialTime.inSeconds - 1;
-      if (seconds < 0) {
-        countDownTimer!.cancel();
-      } else {
-        initialTime = Duration(seconds: seconds);
-        remainingMinutes = initialTime.inMinutes;
-        remainingSeconds = initialTime.inSeconds.remainder(60);
-      }
-      notifyListeners();
+    final seconds = initialTime.inSeconds - 1;
+    if (seconds < 0) {
+      countDownTimer!.cancel();
+    } else {
+      initialTime = Duration(seconds: seconds);
+      remainingMinutes = initialTime.inMinutes;
+      remainingSeconds = initialTime.inSeconds.remainder(60);
+    }
+    notifyListeners();
   }
 }
