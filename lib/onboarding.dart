8b66208d064b140bc4579555c2b8bc06e56a6d6f
Samuel Khongthaw <skthaw1@gmail.com>
2023-06-16 21:02:29 +0530

    integrate onboarding v2

--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:pomodoro/colors.dart';
 
 class Onboarding extends StatefulWidget {
@@ -13,74 +14,68 @@ class Onboarding extends StatefulWidget {
 class _OnboardingState extends State<Onboarding> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.onboardingLight,
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 15.0, bottom: 40),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  SizedBox(
-                    height: 100,
-                  ),
-                  Text(
-                    'Take a step towards productivity, amidst this distracting age.',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    'Get closer to your goals, one pomodoro at a time.',
-                    style: TextStyle(
-                        color: Colors.black54,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  // Text(
-                  //   'Read about why the pomodoro technique works',
-                  //   style: TextStyle(
-                  //       color: Colors.black54,
-                  //       fontSize: 12,
-                  //       decorationColor: Colors.grey,
-                  //       decorationThickness: 2,
-                  //       decoration: TextDecoration.underline,
-                  //       fontWeight: FontWeight.bold),
-                  // ),
-                ],
-              ),
-              const Image(image: AssetImage('assets/onboardBg.jpg')),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    'Next',
-                    style: TextStyle(
-                        color: Colors.red[400],
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.arrow_forward_ios_rounded,
-                      color: Colors.red[400],
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: AppColors.onboardingLight,
+        body: Container(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 15.0, bottom: 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    SizedBox(
+                      height: 100,
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    Text(
+                      'Take a step towards productivity, amidst this distracting age.',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Get closer to your goals, one pomodoro at a time.',
+                      style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                ),
+                const Image(image: AssetImage('assets/onboardBg.jpg')),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Next',
+                      style: TextStyle(
+                          color: Colors.red[400],
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        context.go('/onboard/info');
+                      },
+                      icon: Icon(
+                        Icons.arrow_forward_ios_rounded,
+                        color: Colors.red[400],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
